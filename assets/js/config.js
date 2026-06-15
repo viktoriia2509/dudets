6cd4ed7880c98515bd877c546b15bfb7a1945e78
viktoriia2509 <viktoriia1580@gmail.com>
2026-06-15 09:39:15 +0300

    Delete assets directory

--- assets/js/config.js
+++
@@ -1,8 +0,0 @@
-// Настройки облачной базы. GitHub хранит только файлы сайта, данные сайта лежат отдельно в Firestore.
-window.SITE_CONFIG = {
-  firebaseProjectId: 'dedets-9cc2b',
-  firestoreCollection: 'settings',
-  siteName: 'JOYSTICK',
-  adminDefaultLogin: 'CPJw1r',
-  adminDefaultPassword: 'joystick2024'
-};
